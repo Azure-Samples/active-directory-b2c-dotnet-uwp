d87929746629bdb6245af518d30e7a35b7c19b78
Jenny Ferries <jeferrie@microsoft.com>
2019-08-02 17:33:49 -0700

    remove "s"

--- active-directory-b2c-dotnet-uwp/MainPage.xaml.cs
+++ active-directory-b2c-dotnet-uwp/MainPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;s
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
