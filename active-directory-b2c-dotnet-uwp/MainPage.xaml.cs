e1e055affcc70a203b240edfd59b127180d23e8c
Jenny Ferries <jeferrie@microsoft.com>
2019-08-02 17:32:27 -0700

    update to msal 4.3

--- active-directory-b2c-dotnet-uwp/MainPage.xaml.cs
+++ active-directory-b2c-dotnet-uwp/MainPage.xaml.cs
@@ -2,22 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net.Http.Headers;s
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
